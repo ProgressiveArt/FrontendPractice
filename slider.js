3848ee049123a2ebbdd5391606c1a70aec1d6b8c
Vitaliy Smichkovskiy <32594950+ProgressiveArt@users.noreply.github.com>
2020-01-28 13:38:37 +0300

    update
    Add carusel`ka-slider

--- slider.js
+++ slider.js
@@ -14,42 +14,90 @@ function change() {
     $(this).css('background', 'none');
 }
 
-$(label).click(change);
+
 
 /* Рекурсивный setTimeout – более гибкий метод, чем setInterval. 
    С его помощью последующий вызов может быть задан по-разному в зависимости от результатов предыдущего.*/
-var x = true,
+
+var x = false,
 timeOut = 5000;
 setTimeout(autoslide(),timeOut);
-
+var coin = 0;
+$(label[0]).css('background', 'none');
 
 function autoslide(){
     $(label).css('background', '#264160');
-    if(x==false){
-        right();
-        x = true;
-        $(label[1]).css('background', 'none');
+        if(coin==1){
+            if(x==false){
+                right();
+                
+                $(label[1]).css('background', 'none');
+            }
+            else{
+                left();
+                
+                $(label[0]).css('background', 'none');
+            }
     }
-    else{
-        left();
-        x = false;
-        $(label[0]).css('background', 'none');
-    }
+    
     setTimeout(autoslide, timeOut);
 }
 
 
 /*Движение слайдов*/
+coin = 1;
+var f = true;
+
+
+
+
+
+$(label[1]).click(right); 
+$(label).click(change);
 $(label[0]).click(left);
 
-$(label[1]).click(right);
 
 function left() {
-    $('#s1').css('margin-left', '0%'); 
+    if( x == true){
+    $('#s1').css('left', '0%'); 
+    $('#s2').css('left', '-50%'); 
+    $('.block').css('z-index','6');
+    setTimeout(function() {
+        $('#s2').css('transition', '0s');
+        $('#s2').css('opacity', '0');
+        setTimeout(function(){
+            $('#s2').css('left', '50%');
+            setTimeout(function(){
+                $('#s2').css('opacity', '1');
+                $('#s2').css('transition', '1s');
+                $('.block').css('z-index','4');
+            },20);
+        },10);
+    },600);
+}
+    x = false;
 }
 
 function right() {
-    $('#s1').css('margin-left', '-50%'); 
+    
+    if(x == false){
+    $('#s2').css('left', '0%'); 
+    $('#s1').css('left', '-50%'); 
+    $('.block').css('z-index','6');
+    setTimeout(function() {
+        $('#s1').css('transition', '0s');
+        $('#s1').css('opacity', '0');
+        setTimeout(function(){
+            $('#s1').css('left', '50%');
+            setTimeout(function(){
+                $('#s1').css('opacity', '1');
+                $('#s1').css('transition', '1s');
+                $('.block').css('z-index','4');
+            },20);
+        },10);
+    },600);
+    }
+    x = true;
 }
 
 
